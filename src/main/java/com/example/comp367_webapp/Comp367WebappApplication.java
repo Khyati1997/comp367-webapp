4e64dc22555f264fd484f8776e58aea34dbec636
Khyati1997 <kthakk28@my.centennialcollege.ca>
2025-02-17 17:36:55 -0500

    Initial commit - Spring Boot Web App

---
+++ src/main/java/com/example/comp367_webapp/Comp367WebappApplication.java
@@ -0,0 +1,13 @@
+package com.example.comp367_webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Comp367WebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Comp367WebappApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/comp367_webapp/HomeController.java
@@ -0,0 +1,15 @@
+package com.example.comp367_webapp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping
+    public String homePage() {
+        return "Welcome to COMP367!";
+    }
+}
